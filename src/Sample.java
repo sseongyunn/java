6ebcc4d1becd8ae3798dde4462ab027e0a4789eb
sseongyunn <sseongyunn@korea.ac.kr>
2024-10-14 16:40:59 +0900

    "helloworld.java"

---
+++ src/Sample.java
@@ -0,0 +1,31 @@
+public class Sample {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        long c = 87689090900L;
+        double d = 3.14555454545454545;
+        float f = 3.14F;
+        double g = 314e2;
+        int octal = 023;
+        int hex = 0xC3;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        System.out.println(7%3);
+        System.out.println(3%7);
+
+        int i = 0;
+        int j = 10;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(--j);
+
+    }
+
+
+
+}
